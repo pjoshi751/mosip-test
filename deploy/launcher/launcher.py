f98a13a988438291c175b6fd02f02daf179cf272
Puneet Joshi <puneet@mosip.io>
2019-07-31 02:47:53 -0400

    Clamav added.

--- deploy/launcher/launcher.py
+++ deploy/launcher/launcher.py
@@ -22,17 +22,30 @@ def install_docker():
         
 def install_epel():
     logger.info('Installing  EPEL')
-    subprocess.call('sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm', shell=True)
+    command('sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm')
 
+def install_clamav():
+    logger.info('Installing CLAMAV')
+    command('sudo yum -y install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd')
+    if not os.path.exists('/etc/clamd.d/scan.conf.original'): 
+        command('sudo cp /etc/clamd.d/scan.conf /etc/clamd.d/scan.conf.original') 
+    command('sudo sed -i -e "s/^Example/#Example/" /etc/clamd.d/scan.conf')
+    command('echo "LocalSocket /var/run/clamd.scan/clamd.sock" | sudo tee -a /etc/clamd.d/scan.conf')
+    command('sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf')
+    command('sudo freshclam')
+    command('sudo systemctl start clamd@scan') 
+    command('sudo systemctl enable clamd@scan') 
+  
 def main():
     global logger
     init_logger(logger, 'logs/launcher.log', 10000000, 'info', 2)
 
     give_home_read_permissions() # For various access
-    install_epel()
-    install_docker()
-    install_postgres()
-    init_db()
+    #install_epel()
+    #install_docker()
+    #install_postgres()
+    #init_db()
+    install_clamav()
     logger.info('Install done')
 
 if __name__== '__main__':
