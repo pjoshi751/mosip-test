c33d438769d372156581ac88cb7f8d2369ae4335
Puneet Joshi <puneet@mosip.io>
2019-08-09 21:51:17 -0400

    Comments

--- deploy/launcher/config.py
+++ deploy/launcher/config.py
@@ -1,7 +1,9 @@
+# This file contains the config parameters of the launcher. Inspect the file
+# carefully before running the launcher.  Esp. MOSIP_DIR.
+
 import os
 
 MOSIP_DIR = os.path.join(os.environ['HOME'], 'mosip')
-
 DB_SCRIPTS_PATH = os.path.join(MOSIP_DIR, 'mosip-platform/db_scripts/')
 POSTGRES_PORT = 5432
 COUNTRY_NAME='morocco'  # For LDAP 
