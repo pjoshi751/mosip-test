a571d8e7e8eda4530c5eb6cdcafc3f9f650edcd6
Puneet Joshi <puneet@mosip.io>
2019-08-13 06:18:43 -0400

    Moved to README.

--- start_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Postgres 12 is probably higher for current MOSIP version.  Need to use
-# lower version
-# Run psql as 
-# psql -h localhost -U postgres
-# password: mosip
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=mosip -d -p5432:5432 -v /home/puneet/docker/volumes/postgres:/var/lib/postgresql/data postgres:12
